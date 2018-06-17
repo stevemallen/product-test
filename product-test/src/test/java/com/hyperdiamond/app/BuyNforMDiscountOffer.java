bd0c365819373c08f5062a02f44a17285642e153
Steve Mallen <steven.john.mallen@gmail.com>
2018-06-17 10:15:50 +0100

    Refactor offer classes; they implement the same algorithm so merge to base class.

---
+++ product-test/src/test/java/com/hyperdiamond/app/BuyNforMDiscountOffer.java
@@ -0,0 +1,49 @@
+package com.hyperdiamond.app;
+
+import javax.money.MonetaryAmount;
+
+/**
+ * A discount strategy which implements the algorithm for buying N of a product
+ * for M of a product, where M < N
+ * 
+ * @author Steve
+ *
+ */
+public abstract class BuyNforMDiscountOffer implements DiscountOffer {
+
+	private Product product; // the product that the offer applies to
+	private int groupSize; // N - the group size whose cost is reduced (e.g. 3)
+	private int costSize; // M - the group size which determines actual cost (e.g. 2)
+	
+	public BuyNforMDiscountOffer(Product product) {
+		this.product = product;
+	}
+
+	protected void setCostSize(int costSize) {
+		this.costSize = costSize;
+	}
+
+	protected void setGroupSize(int groupSize) {
+		this.groupSize = groupSize;
+	}
+	
+	@Override
+	public MonetaryAmount apply(Basket basket, MonetaryAmount amount) {
+		// count how many of the product we have
+		// if there is at least one group (the discount applies), then reduce the total cost
+		// by the appropriate amount (costSize * unit cost) * Number of Groups
+		long count = basket.productCount(product);
+		if (count >= groupSize) {
+			long numberOfGroups = count / groupSize;
+			MonetaryAmount costPricePerGroup = product.getPrice().multiply(costSize);
+			MonetaryAmount fullGroupPrice = product.getPrice().multiply(groupSize);
+			MonetaryAmount reductionPerGroup = fullGroupPrice.subtract(costPricePerGroup);
+			MonetaryAmount reduction = reductionPerGroup.multiply(numberOfGroups);
+			return amount.subtract(reduction);
+		} else {
+			// only two products, no discount
+			return amount;
+		}
+	}
+
+}
--- product-test/src/test/java/com/hyperdiamond/app/BuyOneGetOneFree.java
+++ product-test/src/test/java/com/hyperdiamond/app/BuyOneGetOneFree.java
@@ -1,41 +1,17 @@
 package com.hyperdiamond.app;
 
-import javax.money.MonetaryAmount;
-
 /**
  * A Buy One Get One Free offer on a particular Product (e.g. Apples)
  * 
  * @author Steve
  *
  */
-public class BuyOneGetOneFree implements DiscountOffer {
+public class BuyOneGetOneFree extends BuyNforMDiscountOffer {
 
-	private Product product; // the product that the BOGOF applies to
-
-	/**
-	 * Create a BOGOF offer using the prototype product object
-	 * @param apple
-	 */
 	public BuyOneGetOneFree(Product product) {
-		this.product = product;
-	}
-
-	@Override
-	public MonetaryAmount apply(Basket basket, MonetaryAmount amount) {
-		// count how many of the product we have
-		// if there is more than one, then reduce the cost
-		// by the unit value of the product
-		long count = basket.productCount(product);
-		if (count > 1) {
-			long numberOfSets = count / 2; // divide by 2 and round down
-			MonetaryAmount unitPrice = product.getPrice();
-			MonetaryAmount reduction = unitPrice.multiply(numberOfSets);
-			return amount.subtract(reduction);
-		} else {
-			// only one product, no discount
-			return amount;
-		}
-			
+		super(product);
+		setGroupSize(2); // buy 2
+		setCostSize(1);  // get 1 free
 	}
 
 }
